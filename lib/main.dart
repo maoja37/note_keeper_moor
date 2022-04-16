7902cd4a6f58b004a537e7a8fb01f01fa1931173
nuyiwa908 <maoja37@gmail.com>
2022-04-16 23:31:40 +0100

    Fetching Notes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper_moor/database/database.dart';
 import 'package:note_keeper_moor/screens/note_list_page.dart';
+//import provider
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,39 +11,42 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
+    return Provider(
+      create: (_) => AppDatabase(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
 
-        primarySwatch: Colors.deepOrange,
-        textTheme: const TextTheme(
-          headline5: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 24),
-          bodyText2: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-              fontSize: 20),
-          bodyText1: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-              fontSize: 18),
-          subtitle2: TextStyle(
-              fontFamily: 'Sans',
-              fontWeight: FontWeight.normal,
-              color: Colors.black,
-              fontSize: 14),
+          primarySwatch: Colors.deepOrange,
+          textTheme: const TextTheme(
+            headline5: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 24),
+            bodyText2: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                fontSize: 20),
+            bodyText1: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                fontSize: 18),
+            subtitle2: TextStyle(
+                fontFamily: 'Sans',
+                fontWeight: FontWeight.normal,
+                color: Colors.black,
+                fontSize: 14),
+          ),
         ),
+        home: const NoteListPage(),
       ),
-      home: const NoteListPage(),
     );
   }
 }
--- lib/screens/note_list_page.dart
+++ lib/screens/note_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:note_keeper_moor/database/database.dart';
+import 'package:provider/provider.dart';
 
 class NoteListPage extends StatefulWidget {
   const NoteListPage({Key? key}) : super(key: key);
@@ -8,15 +10,48 @@ class NoteListPage extends StatefulWidget {
 }
 
 class _NoteListPageState extends State<NoteListPage> {
+  late AppDatabase database;
+
   @override
   Widget build(BuildContext context) {
+    database = Provider.of<AppDatabase>(context);
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Hello World',
-          style: Theme.of(context).textTheme.headline5,
-        ),
+      body: FutureBuilder<List<NoteData>>(
+        future: _getNoteFromDatabase(),
+        builder: ((context, snapshot) {
+          if (snapshot.hasData) {
+            List<NoteData>? noteList = snapshot.data;
+            if(noteList!=null){
+              if(noteList.isEmpty){
+                return Center(
+                  child:  Text('No Notes Found', style: Theme.of(context).textTheme.bodyText2,),
+                );
+              } else{
+                return noteListUI(noteList);
+              }
+            }
+          } else if (snapshot.hasError) {
+            return Text(
+              '${snapshot.error}',
+              style: Theme.of(context).textTheme.bodyText2,
+            );
+          }
+          return Center(
+            child: Text(
+              'Click on add butotn to add new note',
+              style: Theme.of(context).textTheme.bodyText2,
+            ),
+          );
+        }),
       ),
     );
+  }
+
+  Future<List<NoteData>> _getNoteFromDatabase() {
+    return database.getNoteList();
+  }
+
+  Widget noteListUI(List<NoteData> noteList) {
+    return Container( );
   }
 }
