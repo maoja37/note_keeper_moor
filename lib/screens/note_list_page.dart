94debc6da28ee8fab2d769ca9645aeb8a9745b45
nuyiwa908 <maoja37@gmail.com>
2022-04-16 02:26:49 +0100

    Added fonts

---
+++ lib/screens/note_list_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NoteListPage extends StatefulWidget {
+  const NoteListPage({Key? key}) : super(key: key);
+
+  @override
+  State<NoteListPage> createState() => _NoteListPageState();
+}
+
+class _NoteListPageState extends State<NoteListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello World',
+          style: Theme.of(context).textTheme.headline5,
+        ),
+      ),
+    );
+  }
+}
