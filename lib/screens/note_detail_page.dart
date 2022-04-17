ad28ddc263cdc5b363f732c8af9b16eb1f60970a
nuyiwa908 <maoja37@gmail.com>
2022-04-17 01:09:01 +0100

    started detail screen

--- lib/screens/note_detail_page.dart
+++ lib/screens/note_detail_page.dart
@@ -1,21 +1,36 @@
- import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:note_keeper_moor/database/database.dart';
+import 'package:provider/provider.dart';
 
 class NoteDetailPage extends StatefulWidget {
   final String title;
   final NoteCompanion noteCompanion;
 
-  const NoteDetailPage({ Key? key, required this.title, required this.noteCompanion }) : super(key: key);
-
-
+  const NoteDetailPage(
+      {Key? key, required this.title, required this.noteCompanion})
+      : super(key: key);
 
   @override
   State<NoteDetailPage> createState() => _NoteDetailPageState();
 }
 
 class _NoteDetailPageState extends State<NoteDetailPage> {
+  late AppDatabase database;
+  late TextEditingController titleEditingController;
+  late TextEditingController descriptionEditingController;
+
+@override
+  void initState() {
+    // TODO: implement initState
+    titleEditingController = TextEditingController();
+    descriptionEditingController = TextEditingController();
+    titleEditingController.text = widget.noteCompanion.title.value;
+    descriptionEditingController.text = widget.noteCompanion.description.value;
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
+    database = Provider.of<AppDatabase>(context);
     return Scaffold(
       appBar: _getDetailAppBar(widget.title),
     );
@@ -23,7 +38,42 @@ class _NoteDetailPageState extends State<NoteDetailPage> {
 
   _getDetailAppBar(String title) {
     return AppBar(
-      title: Text(title, style: const TextStyle(color: Colors.black),),
+      backgroundColor: Colors.white,
+      title: Text(
+        title,
+        style: const TextStyle(color: Colors.black),
+      ),
+      leading: IconButton(
+        onPressed: () {
+          Navigator.pop(context);
+        
+        },
+        icon: const Icon(
+          Icons.chevron_left_outlined,
+          color: Colors.black,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+          
+          },
+          icon: const Icon(
+            Icons.save,
+            color: Colors.black,
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+          
+          },
+          icon: const Icon(
+            Icons.delete,
+            color: Colors.black,
+          ),
+        ),
+      
+      ],
     );
   }
 }
